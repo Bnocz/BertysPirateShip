d996c5bc6de3ae123aa6a2ae570a65455f823edb
Darian Gratton <Dariangratton@gmail.com>
2017-05-25 15:39:11 -0700

    Updated Recipe page, star rating system, index, and bug fixes

--- Javascript/mainauth.js
+++ Javascript/mainauth.js
@@ -24,12 +24,17 @@ function PhoenixRecipes() {
     this.userName = document.getElementById('user-name');
     this.signInButton = document.getElementById('sign-in');
     this.signOutButton = document.getElementById('sign-out');
-    this.welcomeUser = document.getElementById('welcomeUser');
+    this.welcomeUser = document.getElementById('welcome-user');
+    this.welcomeHome = document.getElementById('welcome-home');
     this.userName2 = document.getElementById('user-name2');
+    this.signInHome = document.getElementById('sign-in-home');
+    this.signOutHome = document.getElementById('sign-out-home');
 
     // Saves message on form submit.
     this.signOutButton.addEventListener('click', this.signOut.bind(this));
     this.signInButton.addEventListener('click', this.signIn.bind(this));
+    this.signOutHome.addEventListener('click', this.signOut.bind(this));
+    this.signInHome.addEventListener('click', this.signIn.bind(this));
 
     this.initFirebase();
 }
@@ -45,13 +50,13 @@ PhoenixRecipes.prototype.initFirebase = function() {
 };
 
 
-// Signs-in Friendly Chat.
+// Signs-in Phoenix Recipes.
 PhoenixRecipes.prototype.signIn = function() {
     var provider = new firebase.auth.GoogleAuthProvider();
-    this.auth.signInWithPopup(provider);
+    this.auth.signInWithRedirect(provider);
 };
 
-// Signs-out of Friendly Chat.
+// Signs-out of Phoenix Recipes.
 PhoenixRecipes.prototype.signOut = function() {
     this.auth.signOut();
 };
@@ -62,27 +67,45 @@ PhoenixRecipes.prototype.onAuthStateChanged = function(user) {
         // Get profile pic and user's name from the Firebase user object.
         var profilePicUrl = user.photoURL;
         var userName = user.displayName;
+        var userName2 = user.displayName;
 
         // Set the user's profile pic and name.
         this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
         this.userName.textContent = userName;
+        this.userName2.textContent = userName2;
 
         // Show user's profile and sign-out button.
         this.userName.removeAttribute('hidden');
         this.userPic.removeAttribute('hidden');
         this.signOutButton.removeAttribute('hidden');
+        this.signOutHome.removeAttribute('hidden');
+
+        // Show the welcome for the user
+        this.welcomeUser.removeAttribute('hidden');
+
+        // Hide the default welcome when the user isn't signed in
+        this.welcomeHome.setAttribute('hidden', 'true');
 
         // Hide sign-in button.
         this.signInButton.setAttribute('hidden', 'true');
+        this.signInHome.setAttribute('hidden', 'true');
 
     } else { // User is signed out!
         // Hide user's profile and sign-out button.
         this.userName.setAttribute('hidden', 'true');
         this.userPic.setAttribute('hidden', 'true');
         this.signOutButton.setAttribute('hidden', 'true');
+        this.signOutHome.setAttribute('hidden', 'true');
 
         // Show sign-in button.
         this.signInButton.removeAttribute('hidden');
+        this.signInHome.removeAttribute('hidden');
+
+        // Show the welcome for the user when he is logged off
+        this.welcomeHome.removeAttribute('hidden');
+
+        // Hide the welcome for the user
+        this.welcomeUser.setAttribute('hidden', 'true');
     }
 };
 
